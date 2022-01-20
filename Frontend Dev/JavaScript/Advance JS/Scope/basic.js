0cad93a7221bcea9246f3c0eebac864eef03585b
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-01-21 00:28:33 +0530

    Studying JS in Depth with Kyle Simpson

--- Frontend Dev/JavaScript/Advance JS/Scope/basic.js
+++ Frontend Dev/JavaScript/Advance JS/Scope/basic.js
@@ -1,9 +1,9 @@
-console.log("start");
-var minName = "bar";
-function hello() {
-  var anotherName = "Singh";
-}
-hello();
+// console.log("start");
+// var minName = "bar";
+// function hello() {
+//   var anotherName = "Singh";
+// }
+// hello();
 
 //Leakage of Global Variable in non strict Mode
 // var foo = "bar";
@@ -19,3 +19,60 @@ hello();
 // baz(foo);
 
 // In strict mode above code with gave an error
+
+// Lecutre - 3 Scope and Excution
+// var foo = "bar";
+// function bar() {
+//   // Variable Shadowing
+//   var foo = "baz";
+//   function baz(foo) {
+//     foo = "bam";
+//     bam = "yay";
+//   }
+//   baz();
+// }
+// bar();
+// console.log(foo);
+// console.log(bam);
+// baz();
+
+// Shadowing in JS
+// var foo = "bar";
+
+// function bar() {
+//   var foo = "barInsideBarFunction";
+//   console.log(foo);
+// }
+// bar();
+
+// let foo = "bar";
+
+// function bar() {
+//   let foo = "barInsideBarFunction";
+//   console.log(foo);
+// }
+// bar();
+
+// IIFE
+// var foo = "bar";
+// (function() {
+//   var foo = "anotherFO";
+//   console.log(foo);
+// })();
+// console.log(foo);
+
+// {
+//   var foo = "Hello";
+//   let bar = "Hasl";
+// }
+// console.log(foo);
+
+// if (true) {
+//   var foo = "foo";
+// }
+// console.log(foo);
+
+if (true) {
+  let foo = "foo";
+}
+console.log(foo);
