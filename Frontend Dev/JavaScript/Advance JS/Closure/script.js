92017a8c8de794db9653ba1411d0e9463d846c1f
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-02-10 17:17:04 +0530

    Worked on Modern Object Methods

--- Frontend Dev/JavaScript/Advance JS/Closure/script.js
+++ Frontend Dev/JavaScript/Advance JS/Closure/script.js
@@ -41,20 +41,23 @@
 // Simple Setting up Property
 // rabbit.eat = true;
 
-let animal = {
-  isWalking: false,
-  isEating: false,
-  makeWalk() {
-    this.isWalking = true;
-  },
-  startEating() {
-    this.isEating = true;
-  }
-};
+// let animal = {
+//   isWalking: false,
+//   isEating: false,
+//   makeWalk() {
+//     this.isWalking = true;
+//   },
+//   startEating() {
+//     this.isEating = true;
+//   }
+// };
 
-let rabbit = {
-  __proto__: animal
-};
+// let rabbit = {
+//   name: "Dear",
+//   age: 4,
+//   color: "Black",
+//   __proto__: animal
+// };
 
 // let cat = {
 //   __proto__: animal
@@ -75,7 +78,60 @@ let rabbit = {
 // console.log(cat);
 
 // Problem with for...in loop
-for (let key in rabbit) {
-  console.log(key);
-}
-console.log(Object.keys(rabbit));
+// for (let key in rabbit) {
+//   if (rabbit.hasOwnProperty(key)) {
+//     console.log(`This is Rabit's Property ${key}`);
+//   } else {
+//     console.log(`This is Inherited Property ${key}`);
+//   }
+// }
+// console.log(Object.keys(rabbit));
+
+// let hamster = {
+//   stomach: [],
+
+//   eat(food) {
+//     this.stomach.push(food);
+//   }
+// };
+
+// let speedy = {
+//   __proto__: hamster
+// };
+
+// let lazy = {
+//   __proto__: hamster
+// };
+
+// // This one found the food
+// speedy.eat("apple");
+// alert(speedy.stomach); // apple
+
+// // This one also has it, why? fix please.
+// alert(lazy.stomach); // apple
+
+// let animal = {
+//   eat: true
+// };
+
+// function Animal(name) {
+//   this.name = name;
+// }
+// Animal.prototype = animal;
+
+// let rabbit = new Animal("kirat");
+// console.log(rabbit);
+// const pro = Function.prototype;
+// console.log(Function.prototype);
+
+// const author = "Arminder Sing";
+// const stringPro = author.__proto__;
+// author.__proto__.parent = "Jasvir";
+// console.log(author);
+
+// MODERN METHODS TO WORK WITH PROTOTYPE
+// Object.create(prototype, [descriptors])
+let emptyObj = Object.create(null);
+console.log(Object.getPrototypeOf(emptyObj)); //Null
+
+Object.setPrototypeOf(emptyObj, Array.prototype);
