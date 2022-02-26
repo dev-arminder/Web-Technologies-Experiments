634b475d51d29cc24b96504210f4c1a46d2997da
dev-arminder <mail.to.arminder.singh@gmail.com>
2022-02-26 12:19:33 +0530

    Sliding Window Problems

--- Frontend Dev/JavaScript/DSA/Array/Js/index.js
+++ Frontend Dev/JavaScript/DSA/Array/Js/index.js
@@ -175,3 +175,172 @@ function KidsWithCandy(candies, extraCandies) {
 
   return booleanArr;
 }
+
+function maxProduct(nums) {
+  let arr = nums.sort(function(a, b) {
+    return a - b;
+  });
+  console.log(arr);
+  return (arr[nums.length - 1] - 1) * (arr[nums.length - 2] - 1);
+}
+
+function findMaxAverage(nums, k) {
+  let windowStartIdx = 0;
+  let maxSum = -Infinity;
+  let windowSum = 0;
+
+  for (let windowEndIdx = 0; windowEndIdx < nums.length; windowEndIdx++) {
+    windowSum += nums[windowEndIdx];
+
+    if (windowEndIdx >= k - 1) {
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum -= nums[windowStartIdx];
+      windowStartIdx += 1;
+    }
+  }
+  return maxSum;
+}
+
+function minSubArrayLen(target, nums) {
+  // let minLength = Infinity;
+  // let isMatch = false;
+
+  // for (let i = 0; i < nums.length; i++) {
+  //   let currentRunningSum = 0;
+  //   let currentRunninglen = 0;
+  //   currentRunningSum += nums[i];
+  //   currentRunninglen += 1;
+  //   if (currentRunningSum >= target) {
+  //     minLength = Math.min(minLength, currentRunninglen);
+  //     isMatch = true;
+  //     return minLength;
+  //   }
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     currentRunningSum += nums[j];
+  //     currentRunninglen += 1;
+
+  //     if (currentRunningSum >= target) {
+  //       minLength = Math.min(minLength, currentRunninglen);
+  //       isMatch = true;
+  //     }
+  //   }
+  // }
+
+  // return isMatch ? minLength : 0;
+
+  // More Optimized Approch
+  let runningSum = 0;
+  let minLength = Infinity;
+  let isMatch = false;
+  let windowLength = 0;
+  let windowStartIdx = 0;
+
+  for (let windowEndIdx = 0; windowEndIdx < nums.length; windowEndIdx++) {
+    runningSum += nums[windowEndIdx];
+    windowLength += 1;
+    // Close Condition For Single Elemenet
+    if (runningSum >= target) {
+      isMatch = true;
+      minLength = Math.min(windowLength, minLength);
+    }
+
+    // Shrinking Window Length
+    while (runningSum >= target) {
+      runningSum -= nums[windowStartIdx];
+      windowStartIdx += 1;
+      windowLength -= 1;
+      if (runningSum >= target) {
+        isMatch = true;
+        minLength = Math.min(windowLength, minLength);
+      }
+    }
+  }
+
+  return isMatch ? minLength : 0;
+}
+
+function longestSubstringWithKdistinct(str, k) {
+  let dChar = 0;
+  let dMap = {};
+  let currentLength = 0;
+  let maxLength = -Infinity;
+  let subStr = "";
+
+  for (let windowEndIdx = 0; windowEndIdx < str.length; windowEndIdx++) {
+    // Create Map And Counting Distinct Char in Particular SubString
+    if (dMap[str[windowEndIdx]]) {
+      dMap[str[windowEndIdx]] += 1;
+    } else {
+      dMap[str[windowEndIdx]] = 1;
+      dChar += 1;
+    }
+    currentLength += 1;
+    // Check For Unique Char
+    if (dChar === k) {
+      maxLength = Math.max(maxLength, currentLength);
+    }
+    // Create SubStr
+    subStr += str[windowEndIdx];
+    while (dChar > k) {
+      let fChar = subStr[0];
+      subStr = subStr.slice(1);
+      currentLength -= 1;
+      if (dMap[fChar] === 1) {
+        delete dMap[fChar];
+        dChar -= 1;
+      } else {
+        dMap[fChar] -= 1;
+      }
+    }
+
+    // // Create SubStr
+    // subStr += str[windowEndIdx];
+    // currentLength += 1;
+
+    // if(dChar === k){
+
+    // }
+
+    // For First Char;
+  }
+  return maxLength;
+}
+
+function totalFruit(fruits) {
+  let maxFruits = -Infinity;
+
+  // Generate Possible SubArray
+  // for (let i = 0; i < fruits.length; i++) {
+  //   // Keep it 2 - Problem Constiants
+  //   let dChar = 0;
+  //   let subArrSum = 0;
+  //   let dMap = {};
+
+  //   dChar += 1;
+  //   dMap[fruits[i]] = 1;
+  //   subArrSum += 1;
+  //   maxFruits = Math.max(maxFruits, subArrSum);
+  //   for (let j = i + 1; j < fruits.length; j++) {
+  //     if (dMap[fruits[j]]) {
+  //       //Already There
+  //       dMap[fruits[j]] += 1;
+  //       subArrSum += 1;
+  //     } else {
+  //       dMap[fruits[j]] = 1;
+  //       dChar += 1;
+  //       subArrSum += 1;
+  //     }
+  //     if (dChar <= 2) {
+  //       maxFruits = Math.max(maxFruits, subArrSum);
+  //     }
+  //     if (dChar > 2) {
+  //       break;
+  //     }
+  //   }
+  // }
+  // return maxFruits;
+}
+console.log(totalFruit([3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4])); //5
+console.log(totalFruit([1, 2, 1])); //3,We can pick from all 3 trees.
+console.log(totalFruit([0, 1, 2, 2])); //3,We can pick from trees [1,2,2].If we had started at the first tree, we would only pick from trees [0,1].
+console.log(totalFruit([1, 2, 3, 2, 2])); //4,We can pick from trees [2,3,2,2]. If we had started at the first tree, we would only pick from trees [1,2].
